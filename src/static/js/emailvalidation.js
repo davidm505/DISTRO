8b5922aeb462906a1a8349a04cb45f042f885a81
David Morales <damorales310@gmail.com>
2020-04-18 14:30:07 -0600

    Added form action variable.

--- src/static/js/emailvalidation.js
+++ src/static/js/emailvalidation.js
@@ -1,20 +1,20 @@
 let queryString = window.location.href;
 let url = queryString.slice(-7);
-let email = url.slice(5)
-let id = url.slice(-1)
+let email = url.slice(5);
+let id = url.slice(-1);
 
+$(document).ready(function () {
+  $(function () {
+    $('[data-toggle="popover"').tooltip();
+  });
+});
 
-$(document).ready(function (){
-    
-    $(function () {
-        $('[data-toggle="popover"').tooltip();
-      })
-})
+let form = document
+  .querySelector("form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-let form = document.querySelector("form").addEventListener('submit', function(event){
-
-    event.preventDefault()
-
+    let formAction = this.action;
     let emailSelect = document.querySelector(".custom-select");
 
     let dict = {};
@@ -25,63 +25,66 @@ let form = document.querySelector("form").addEventListener('submit', function(ev
     dict["shootDay"] = document.querySelector("#shoot-day").value;
     dict["gb"] = document.querySelector("#gb").value;
     dict["trt"] = document.querySelector("#trt").value;
-    dict["cm"] = document.querySelector('#c-masters').value;
+    dict["cm"] = document.querySelector("#c-masters").value;
     dict["sm"] = document.querySelector("#s-masters").value;
     dict["email"] = emailSelect.options[emailSelect.selectedIndex].text;
 
     // Check for any items in form that were not filled out.
-    for (key in dict){
+    for (key in dict) {
+      let value = dict[key];
 
-        let value = dict[key];
-    
-        if (value == ""){
-            completeForm = false;
-            break;
-        }
+      if (value == "") {
+        completeForm = false;
+        break;
+      }
     }
 
     if (completeForm == true) {
-
-        $.post("/generator/" + dict["email"].toLowerCase() + "/" + id, 
+      $.post(
+        formAction,
         {
-            "ep": dict["ep"],
-            "shoot-day": dict["shootDay"],
-            "gb": dict["gb"],
-            "trt": dict["trt"],
-            "c-masters": dict["cm"],
-            "s-masters": dict["sm"],
-            "email": dict["email"]
-        }, 
-        function(response){
-                
-            if (!response) {
-                alert("Hey no response");
-                event.preventDefault();
-            }
+          ep: dict["ep"],
+          "shoot-day": dict["shootDay"],
+          gb: dict["gb"],
+          trt: dict["trt"],
+          "c-masters": dict["cm"],
+          "s-masters": dict["sm"],
+          email: dict["email"],
+        },
+        function (response) {
+          if (!response) {
+            alert("Hey no response");
+            event.preventDefault();
+          }
 
-            // Display email contianer
-            $('.email-container').fadeIn();
-            document.querySelector(".email-container").style.display = "grid";
-    
-            document.querySelector(".email-subject").innerHTML = response["subject"] + "<br>";
-            let body = document.querySelector(".email-body");
-            body.innerHTML = response["body"][0] + "<br>" + response["body"][1] + "<br><br>" + response["body"][2];
-    
-            document.querySelector(".email-distro").innerHTML = response["distro"];
-        })
+          // Display email contianer
+          $(".email-container").fadeIn();
+          document.querySelector(".email-container").style.display = "grid";
+
+          document.querySelector(".email-subject").innerHTML =
+            response["subject"] + "<br>";
+          let body = document.querySelector(".email-body");
+          body.innerHTML =
+            response["body"][0] +
+            "<br>" +
+            response["body"][1] +
+            "<br><br>" +
+            response["body"][2];
+
+          document.querySelector(".email-distro").innerHTML =
+            response["distro"];
+        }
+      );
     }
-
-
-})
+  });
 
 function copy(element) {
-
-   if (window.getSelection) {
-        var range = document.createRange();
-         range.selectNode(document.querySelector(element));
-         window.getSelection().removeAllRanges();
-         window.getSelection().addRange(range);
-         document.execCommand("copy");
-         window.getSelection().removeAllRanges();
-    }
-};
+  if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.querySelector(element));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  }
+}
